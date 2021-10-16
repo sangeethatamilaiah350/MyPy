4e4ffd36c2703bb261aaf224ae835cfceb955a66
SANGEETHA <66882971+sangeethatamilaiah350@users.noreply.github.com>
2021-10-16 23:49:51 +0530

    Update SearchIn2Darray.py

--- leetcode/array/SearchIn2Darray.py
+++ leetcode/array/SearchIn2Darray.py
@@ -1,5 +1,7 @@
 #only row is sorted
 
+
+#leetcode
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         low=0
@@ -18,5 +20,21 @@ class Solution:
                 high=mid-1
                 
         return False
-            
-        
+    
+    
+#both row and col is sorted
+class Solution:
+	def matSearch(self,mat, N, M, X):
+		# Complete this function
+		i=0
+		j=M-1
+		while j!=-1   and  i!=N:
+		    val=mat[i][j]
+		   # print(val)
+		    if val==X:
+		        return 1
+		    elif X<val:
+		        j-=1
+		    else:
+		        i+=1
+		return 0
