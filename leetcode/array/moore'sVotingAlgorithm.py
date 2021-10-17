3da328b291369e2f24d28e0b29bedf325a2e53d5
SANGEETHA <66882971+sangeethatamilaiah350@users.noreply.github.com>
2021-10-17 22:22:35 +0530

    Update moore'sVotingAlgorithm.py

--- leetcode/array/moore'sVotingAlgorithm.py
+++ leetcode/array/moore'sVotingAlgorithm.py
@@ -1,4 +1,4 @@
-#assuming that there is always a majority element in the array
+#assuming that there is always a majority element in the array  {leetcode}
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         maj=0
@@ -11,3 +11,26 @@ class Solution:
             else:
                 c-=1
         return maj
+
+#assuming that may or may not be  a majority element in the array  {geek for geek}    
+    class Solution:
+    def majorityElement(self, A, N):
+        #Your code here
+        c=0
+        maj=-1
+        for i in A:
+            if c==0:
+                maj=i
+            if i==maj:
+                c+=1
+            else:
+                c-=1
+        c=0
+        for i in A:
+            if i==maj:
+                c+=1
+        if c>(N//2):#important important
+            return maj
+        else:
+            return -1
+        
